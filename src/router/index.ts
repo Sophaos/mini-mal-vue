0576689b3afadefed28c8a1c8f28f8c8b9538d48
Son-Thang Pham <54873061+Sophaos@users.noreply.github.com>
2024-01-05 02:41:37 -0500

    setting up file architecture with views first

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,35 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/animes',
+      name: 'animes',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../animes/views/AnimeListView.vue')
+    },
+    {
+      path: '/season',
+      name: 'season',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../season/views/SeasonListView.vue')
+    },
+    {
+      path: '/mangas',
+      name: 'mangas',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../mangas/views/MangaListView.vue')
     }
   ]
 })
